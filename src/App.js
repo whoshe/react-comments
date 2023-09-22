d64cd51087fc912b2a15d34347ca10ca52c850da
Changwoo Nam <whoshe@funnyplan.com>
2023-09-22 17:02:38 +0900

    Title changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Comments from "./comments/Comments";
 const App = () => {
   return (
     <div>
-      <h1>Hello monsterlessons</h1>
+      <h1>React Comments Test</h1>
       <Comments
         commentsUrl="http://localhost:3004/comments"
         currentUserId="1"
